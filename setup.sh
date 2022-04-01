290ae44df6fa023e40836bc483ae518764188d65
sidhaniedotcom <102436523+sidhaniedotcom@users.noreply.github.com>
2022-04-02 01:13:51 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,14 +15,14 @@ figlet -f big FINISHED !!! | lolcat
 echo ""
 echo ""
 echo " SUBSCRIBE MY CHANNEL 🔔 " | lolcat
-xdg-open https://www.youtube.com/channel/HYDRAGAMING4U
+xdg-open https://www.youtube.com/channel/sidhaniedotcom
 echo ""
 sleep 10 
 echo ""
-echo " code recorded by tech-cochi "
+echo " Created By Sidhanie "
 sleep 5
 sleep 10
 echo " create account on cashkaro.com "
 echo " set your browser "
-termux-open-url https://cashkaro.com?r=8026255&fname=Rixon
+termux-open-url https://cashkaro.com
 echo ""
